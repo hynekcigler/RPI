3206a9894d5df6d70b0d7b20b35c349f0fab940f
Hynek Cígler <hynek.cigler@mail.muni.cz>
2016-10-02 19:51:48 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -3,20 +3,42 @@ library(psych)
 
 
 shinyServer(function(input, output, session) {
+
+  test <- reactive({input$vlastni})
   
 
-  output$RPI <- renderText({
-    if (input$ROC == 3) {
-      M <- 490.4843591
-    } else if (input$ROC == 4) {
-      M <- 499.3864715
-    } else {
-      M <- 507.7411568
-    }
-    x <- input$x/100
-    
-    RPI <- round(1/(1+exp(-(log(3)/10*(input$W1-M)-log((1-x)/x))))*100, digits=0)
-    paste(RPI, input$x, sep="/")
-  })
+    output$RPI <- renderText({ ## if TIM test is used
+      if (test() == "tim") {
+        
+        if (input$ROC == 3) {
+          M <- 490.4843591 ## mean for grade 3
+        } else if (input$ROC == 4) {
+          M <- 499.3864715 ## mean for grade 4
+        } else {
+          M <- 507.7411568 ## mean for grade 5
+        }
+        x <- input$x/100 ## percent correct to probability
+        
+        RPI <- round(1/(1+exp(-(log(3)/10*(input$W1-M)-log((1-x)/x))))*100, digits=0) ## RPI estimation
+        paste(RPI, input$x, sep="/") ## format RPI
+      } else {
+        output$RPI <- renderText({ ## if user test is used
+          if (input$skala=="logit") {
+            a <- 1 ## discrimination parameter for logit scale
+          } else if (input$skala=="wscore") {
+            a <- log(3)/10 ## discrimination parameter for W-score scale
+          } else {
+            a <- input$a ## user discrimination parameter
+          }
+          x <- input$x/100
+          RPI <- round(1/(1+exp(-(a*(input$W1-input$M)-log((1-x)/x))))*100, digits=0) ## RPI estimation
+          paste(RPI, input$x, sep="/") ## format RPI
+        })
+      }
+      
+      
+    })
+
+
   
 })
--- ui.R
+++ ui.R
@@ -6,9 +6,31 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     h3("Zadání"),
-    numericInput("W1", "Dosažené W-skóre žáka", 500, step=1, width="300px"),
-    numericInput("ROC", "Ročník", 3, min=3, max= 5, step=1, width="300px"),
-    numericInput("x", "Základ pro RPI index (v %)", 10, min=1, max= 99, width="300px")
+    selectInput(
+      "vlastni", "Jaký test chcete zadat?",
+      c(TIM="tim",
+        vlastní = "muj")),
+    
+    conditionalPanel(condition = "input.vlastni == 'tim'",
+                     
+      numericInput("W1", "Dosažené W-skóre žáka", 500, step=1, width="300px"),
+      numericInput("ROC", "Ročník", 3, min=3, max= 5, step=1, width="300px"),
+      numericInput("x", "Základ pro RPI index (v %)", 10, min=1, max= 99, width="300px")
+      ),
+    conditionalPanel(condition = "input.vlastni == 'muj'",
+                     numericInput("W1", "Dosažené IRT skóre", 500, step=1, width="300px"),
+                     numericInput("M", "Průměrné skóre v kategorii", 490, width="300px"),
+                     numericInput("x", "Základ pro RPI index (v %)", 10, min=1, max= 99, width="300px"),
+                     selectInput(
+                       "skala", "škála Raschova modelu",
+                       c(logitová="logit",
+                         W_škála = "wscore",
+                         jiná = "jine")),
+                     conditionalPanel(condition = "input.skala == 'jine'",
+                                      numericInput("a", "Diskriminační parametr", 1, width="300px")
+                                      )
+                     
+    )
     ), 
   
   
